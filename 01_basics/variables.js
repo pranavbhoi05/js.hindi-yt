40cb05ab6d47a875e8f5a8cd991eb27d9f4c2ab6
pranavbhoi05 <pranavbhoir182000@gmail.com>
2024-12-02 18:53:26 +0000

    same

---
+++ 01_basics/variables.js
@@ -0,0 +1,20 @@
+const accountId="12345"
+let accountEmail="pb@pb.com"
+var accountPassword="212121"
+accountCity="pune"
+
+//accountId="2"// not allow to use cause constant dont change
+
+accountEmail="meme.com"
+accountPassword="69"
+accountCity="mumbai"
+
+
+console.log(accountId);
+
+// never use var keyword. we use let insted cause var can cause trouble(let is similar as var)//
+
+//we wants to declare multiple variable so that we use table so we can add all variable in same line//
+ 
+console.table([accountId;accountEmail;accountPassword;accountCity])
+
