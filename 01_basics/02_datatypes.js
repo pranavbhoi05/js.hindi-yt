91731fdc9e2f5e247e7302692f56c291a147c9f6
pranavbhoi05 <pranavbhoir182000@gmail.com>
2024-12-03 08:01:13 +0000

    datatype

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -2,4 +2,27 @@
 
 // alert(3 + 3) // we are using nodejs, not browser
 
-console.log(3+3);
+console.log(3+3) 
+console.log("pranav")
+
+let name="pranav"
+let age=18
+let isloogedin=false
+
+//number => 2 to  power 53
+//bigint
+// string = ""
+// boolean = true/false
+// null => standalone value
+// undefined =>    // technically value is not present but sooner there will be
+// symbol => unique
+
+
+// object
+
+console.log(typeof "pranav");
+console.log(typeof 18);
+console.log(typeof undefined);  //undefined
+console.log(typeof null);       //object
+
+ 
