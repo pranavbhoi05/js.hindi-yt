d085a6673e40e447131d901e6eb8a360a1324a00
pranavbhoi05 <pranavbhoir182000@gmail.com>
2024-12-04 10:33:31 +0000

    done

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,77 @@
+let score = "hitesh"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+
+let value = 3
+let negvalue = -value
+
+// console.log(negval)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2%2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+
+let str1 = "hello"
+let str2 = " pranav"
+
+let str3 = str1 + str2
+
+// console.log(str3);
+
+
+// console.log("2" + 2);
+// console.log(2 + "2");
+// console.log("2" + 2 + 2)
+// console.log(2 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+ 
+let a = 2
+let b = a++
+console.log(a);
+console.log(b);
+
+// If used postfix, with operator after operand (for example, x++), the increment operator increments and returns the value before incrementing.
+
+let x = 2
+let y = ++x
+console.log(x);
+console.log(y);
+
+// If used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing.
+
+
+
+
