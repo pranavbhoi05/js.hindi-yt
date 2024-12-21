cbcc108a11a70a8440fcb5bd2e2a4b43c1da410e
pranavbhoi05 <pranavbhoir182000@gmail.com>
2024-12-21 08:38:51 +0000

    j

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,4 +1,28 @@
 //  arrays
 
 
-const  
+
+
+const  myarray = [0,1,2,3,4,5]
+const myhero = ["Pranav","cal`el","garou"]
+
+const  myarray2 = new Array(1,2,3,4)  //automatically convert in []
+console.log(myarray[2]);
+
+
+//array methods
+
+// myarray.push(6)   //push adds value in array from last 
+// myarray.push(7)
+// myarray.pop()     //pop delete the value in array from last
+
+// myarray.unshift(7)    // add value from start of array
+// // myarray.shift()    //delete value from start of array 
+
+// console.log(myarray.includes(5));//we show its boolean ornot
+// console.log(myarray.indexOf(3));
+
+const newarray = myarray.join()
+
+console.log(myarray);
+console.log(typeof newarray);
