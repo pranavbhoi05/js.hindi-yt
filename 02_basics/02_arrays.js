a012b2572b0756fd4d7ef0884642ca14bbbb0b34
pranavbhoi05 <pranavbhoir182000@gmail.com>
2025-01-18 02:06:57 +0530

    Update 02_arrays.js

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -34,7 +34,7 @@ const all_new_heros = [...marvel_heros, ...dc_heros]
 // console.log(all_new_heros);
 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
-
+The flat() method of Array instances creates a new array with all sub-array elements concatenated into it recursively up to the specified depth.
 const real_another_array = another_array.flat(Infinity)
 console.log(real_another_array);
 
