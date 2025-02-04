8ff42c3fc2a96c8f8e7ddfe825e2c546c06fad12
pranavbhoi05 <pranavbhoir182000@gmail.com>
2025-02-04 15:20:54 +0530

    Create two.js

---
+++ 05_iterations/two.js
@@ -0,0 +1,25 @@
+ // (while loop) : in this condition u need to declear variable on your own
+
+ let i = 1
+while (i <= 10) {
+    console.log(`print value ${i}`);
+    i = i + 1
+}
+
+//**********************************************8 */ 
+// (while loop )by using array
+const myar = ["pran","sam","lam","tan"]
+let arr = 0
+while (arr < myar.length) {
+    console.log(`val of i is ${myar[arr]}`);
+    arr = arr + 1
+}
+
+// **************************************************
+//  do-while loop
+
+let j = 0
+ do {
+    console.log(`value is ${j}`);
+    j++
+ } while (j <= 10);
